11b8197f18c545deb13761303f31c29cf7a595ad
Nicolas <nicodupriez@hotmail.com>
2020-04-26 11:58:41 +0200

    Adding Fake Data with Faker

--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -5,22 +5,51 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+use App\Entity\Category;
+use App\Entity\Comment;
 use App\Entity\Trick;
 
 class TrickFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i=1; $i <= 10; $i++){
-            $trick = new Trick();
-            $trick->setName("Titre de la figure n°$i")
-                ->setDescription("<p>Description de la figure n°$i</p>")
-                ->setImage("http://placehold.it/350x150")
-                ->setCreatedAt(new \DateTime())
-                ->setCategory("Categorie de la figure n°$i")
-                ->setVideo("http://placehold.it/350x150");
-            $manager->persist($trick);
+        $faker = \Faker\Factory::create('fr_FR');
 
+        for ($i = 1; $i <= 3; $i++) {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            for ($j = 1; $j <= mt_rand(4, 6); $j++) {
+                $trick = new Trick();
+
+                $content = '<p>'.join($faker->paragraphs(5), '</p><p>').'</p>';
+
+                $trick->setName($faker->sentence())
+                    ->setDescription($content)
+                    ->setImage($faker->imageUrl())
+                    ->setVideo($faker->imageUrl())
+                    ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                    ->setCategory($category);
+
+                $manager->persist($trick);
+
+                for ($k = 1; $k >= mt_rand(4, 10); $k++) {
+                    $comment = new Comment();
+                    $content2 = '<p>'.join($faker->paragraphs(2), '</p><p>').'</p>';
+
+                    $days = (new \DateTime())->diff($trick->getCreatedAt())->days;
+
+                    $comment->setAuthor($faker->name)
+                        ->setContent($content2)
+                        ->setCreatedAt($faker->dateTimeBetween('-'.$days.' days'))
+                        ->setTrick($trick);
+
+                    $manager->persist($comment);
+                }
+            }
         }
 
         $manager->flush();
