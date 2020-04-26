ad7143ceae8e7e58c30d5b581446c4337bad6055
Nicolas <nicodupriez@hotmail.com>
2020-04-26 12:30:16 +0200

    Added Category list in Trick edit/create

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Trick;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,9 +15,11 @@ class TrickType extends AbstractType
     {
         $builder
             ->add('name')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title'
+            ])
             ->add('description')
-            ->add('category')
-            //->add('createdAt')
             ->add('image')
             ->add('video')
         ;
