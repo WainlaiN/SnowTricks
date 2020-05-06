5b20ab405194750be871420b152ae696a690d0cc
Nicolas <nicodupriez@hotmail.com>
2020-05-06 18:05:16 +0200

    Added HomeController/Editing TrickController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TrickRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="trick")
+     */
+    public function index(TrickRepository $repo)
+    {
+        $tricks = $repo->findAll();
+
+        return $this->render(
+            'trick/index.html.twig',
+            [
+                'controller_name' => 'TrickController',
+                'tricks' => $tricks,
+            ]
+        );
+    }
+
+}
--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -7,7 +7,6 @@ use App\Entity\Comment;
 use App\Form\CommentType;
 use App\Form\TrickType;
 use Doctrine\ORM\EntityManagerInterface;
-//use MongoDB\Driver\Manager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,31 +16,13 @@ use App\Entity\Trick;
 
 class TrickController extends AbstractController
 {
-    /**
-     * @Route("/", name="trick")
-     */
-    public function index(TrickRepository $repo)
-    {
-        $tricks = $repo->findAll();
-
-        return $this->render(
-            'trick/index.html.twig',
-            [
-                'controller_name' => 'TrickController',
-                'tricks' => $tricks,
-            ]
-        );
-    }
 
     /**
      * @Route("/trick/new", name="trick_create")
-     * @Route("/trick/{id}/edit", name="trick_edit")
      */
-    public function form(Trick $trick = null, Request $request, EntityManagerInterface $manager)
+    public function create(Request $request, EntityManagerInterface $manager)
     {
-        if (!$trick) {
-            $trick = new Trick();
-        }
+        $trick = new Trick();
 
         $form = $this->createForm(TrickType::class, $trick);
 
@@ -56,6 +37,29 @@ class TrickController extends AbstractController
             $manager->flush();
 
             return $this->redirectToRoute('trick_show', ['id' => $trick->getId()]);
+
+        }
+
+        return $this->render(
+            'trick/create.html.twig',
+            [
+                'formTrick' => $form->createView(),
+                'editMode' => $trick->getId() !== null,
+            ]
+        );
+
+    }
+
+    /**
+     * @Route("/trick/{id}/edit", name="trick_edit")
+     */
+    public function edit(Trick $trick, Request $request, EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(TrickType::class, $trick);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
 
         }
 
@@ -78,9 +82,10 @@ class TrickController extends AbstractController
         $form = $this->createForm(CommentType::class, $comment);
 
         return $this->render(
-            'trick/show.html.twig', [
+            'trick/show.html.twig',
+            [
                 'trick' => $trick,
-                'commentForm' => $form->createView()
+                'commentForm' => $form->createView(),
             ]
         );
     }
