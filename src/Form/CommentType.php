9dbe6890c6a0027278bd7f5ecd49857237cf2486
Nicolas <nicodupriez@hotmail.com>
2020-05-06 09:57:59 +0200

    Fixed Comment class with user class entityType

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -2,17 +2,24 @@
 
 namespace App\Form;
 
-use App\Entity\Comment;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use App\Entity\User;
+use App\Entity\Comment;
+
 
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('author')
+            ->add('user', EntityType::class, [
+                'class' => user::class,
+                'choice_label' => 'username'])
+            //->add('user')
             ->add('content')
         ;
     }
