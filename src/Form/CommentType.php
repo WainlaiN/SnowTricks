28595148db13b4ff1356dc6aab022aa90d623a8e
Nicolas <nicodupriez@hotmail.com>
2020-06-10 20:42:45 +0200

    Working on comments template"

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -6,6 +6,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -20,7 +21,7 @@ class CommentType extends AbstractType
         $builder
             ->add(
                 'content',
-                TextareaType::class,
+                TextType::class,
                 [
                     'attr' => [
                         'placeholder' => 'Votre commentaire',
