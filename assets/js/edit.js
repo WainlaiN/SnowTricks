46f26e263ddbbb783cb43335b965eb80b05c6dc7
Nicolas <nicodupriez@hotmail.com>
2020-06-16 18:00:51 +0200

    Working on javascript to delete image

--- assets/js/edit.js
+++ assets/js/edit.js
@@ -5,19 +5,23 @@ window.onload = () => {
     console.log(links)
 
     //on boucle sur links
-    for (link of links){
+    for (link of links) {
         //on écoute le clic
         link.addEventListener("click", function (e) {
             //on empeche le click
             e.preventDefault()
 
             //on demande confirmation
-            if (confirm("Voulez-vous supprimer cette image ?")){
-
+            if (confirm("Voulez-vous supprimer cette image ?")) {
+                // on envoie une requete Ajax vers le href du lien avec la methode DELETE
+                fetch(this.getAttribute("href"), {
+                    method: "DELETE",
+                    headers: {
+                        "X-Requested-With": "XMLHttpRequest",
+                        "Content-Type": "application/json"
+                    }
+                })
             }
-
         })
     }
-
-
 }
