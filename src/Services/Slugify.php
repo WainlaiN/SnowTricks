d3306b40ba934300a4d2374788222a1e1aa36107
Nicolas <nicodupriez@hotmail.com>
2020-06-17 15:00:15 +0200

    Use simple slugify

--- src/Services/Slugify.php
+++ src/Services/Slugify.php
@@ -7,34 +7,10 @@ class Slugify
     public static function slugify($str, $delimiter = '-')
     {
         $str = strtolower(trim($str));
-        $str = preg_replace('/[^a-z0-9-]/', '-', $str);
+        $str = preg_replace('/[^A-Za-z0-9-]+/', '-', $str);
         $str = preg_replace('/-+/', "-", $str);
         return rtrim($str, '-');
 
-
     }
-    /**{
-        $text = strtolower(
-            trim(
-                preg_replace(
-                    '/[\s-]+/',
-                    $delimiter,
-                    preg_replace(
-                        '/[^A-Za-z0-9-]+/',
-                        $delimiter,
-                        preg_replace(
-                            '/[&]/',
-                            'and',
-                            preg_replace('/[\']/', '', iconv('UTF-8', 'ASCII//TRANSLIT', $text))
-                        )
-                    )
-                ),
-                $delimiter
-            )
-        );
-
-        return $text;
-
-    }**/
 
 }
