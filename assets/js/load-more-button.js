73e4dba1f298a3b6e289f735e11bd6f02aae01a9
Nicolas <nicodupriez@hotmail.com>
2020-06-18 09:25:55 +0200

    fix load more button

--- assets/js/load-more-button.js
+++ assets/js/load-more-button.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(".moreBox").slice(0, 4).show();
-    if ($(".blogBox:hidden").length != 0) {
+    if ($(".moreBox:hidden").length != 0) {
         $("#loadMore").show();
     }
     $("#loadMore").on('click', function (e) {
