a82c951bccdfeb73c3620e63593a1a9966c51234
Nicolas <nicodupriez@hotmail.com>
2020-06-24 18:29:41 +0200

    working on loadmore jquery

--- assets/js/load-more-button.js
+++ assets/js/load-more-button.js
@@ -1,23 +1,24 @@
-/**$(document).ready(function () {
+$(document).ready(function () {
     $(".moreBox").slice(0, 4).show();
     if ($(".moreBox:hidden").length != 0) {
         $("#loadMore").show();
     }
     $("#loadMore").on('click', function (e) {
         //e.preventDefault();
-        $("#buttonUp").show();
+        $("#showLess").show();
         $(".moreBox:hidden").slice(0, 4).slideDown();
         if ($(".moreBox:hidden").length == 0) {
             $("#loadMore").fadeOut('slow');
         }
     });
-    $("#buttonUp").on('click', function (e) {
-        $("#buttonUp").hide();
+    $("#showLess").on('click', function (e) {
+        $("#showLess").hide();
         $(".moreBox").slice(4, 8).hide();
 
     });
-});**/
+});
 
+/**
 $(document).ready(function () {
     // Load the first 3 list items from another HTML file
     //$('#myList').load('externalList.html li:lt(3)');
@@ -28,6 +29,7 @@ $(document).ready(function () {
     $('#loadMore').click(function () {
         $('#showLess').show();
         shown = $('#moreBox:visible').length+4;
+        console.log(shown)
         if(shown< items) {$('#moreBox:lt('+shown+')').show();}
         else {$('#myList li:lt('+items+')').show();
             $('#loadMore').hide();
@@ -36,6 +38,6 @@ $(document).ready(function () {
     $('#showLess').click(function () {
         $('#myList li').not(':lt(3)').hide();
     });
-});
+});**/
 
 
