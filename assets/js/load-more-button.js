c17aecd21a81621b168a942dcf8bbffa8400ac0c
Nicolas <nicodupriez@hotmail.com>
2020-06-25 08:53:49 +0200

    clean

--- assets/js/load-more-button.js
+++ assets/js/load-more-button.js
@@ -17,27 +17,3 @@ $(document).ready(function () {
 
     });
 });
-
-/**
-$(document).ready(function () {
-    // Load the first 3 list items from another HTML file
-    //$('#myList').load('externalList.html li:lt(3)');
-    $(".moreBox").slice(0, 4).show();
-    $('#showLess').hide();
-    var items =  10;
-    var shown =  4;
-    $('#loadMore').click(function () {
-        $('#showLess').show();
-        shown = $('#moreBox:visible').length+4;
-        console.log(shown)
-        if(shown< items) {$('#moreBox:lt('+shown+')').show();}
-        else {$('#myList li:lt('+items+')').show();
-            $('#loadMore').hide();
-        }
-    });
-    $('#showLess').click(function () {
-        $('#myList li').not(':lt(3)').hide();
-    });
-});**/
-
-
