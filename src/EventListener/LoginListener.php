44bf03290e1fcdb361e4710796d8509467e34310
Nicolas <nicodupriez@hotmail.com>
2020-07-02 14:52:04 +0200

    fix left arrow image

--- src/EventListener/LoginListener.php
+++ src/EventListener/LoginListener.php
@@ -3,28 +3,41 @@
 
 namespace App\EventListener;
 
-
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\RedirectController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Test\Constraint\ResponseIsRedirected;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class LoginListener
 {
     private $session;
+    private $em;
 
-    public function __construct(SessionInterface $session)
+    public function __construct(EntityManagerInterface $em, SessionInterface $session)
     {
+        $this->em = $em;
         $this->session = $session;
     }
 
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
     {
+        // Get the User entity.
+        $user = $event->getAuthenticationToken()->getUser();
+
+        //check if token activation token is still present
         if ($user->getActivationToken()) {
 
             $this->session->getFlashBag()->add('danger', 'Vous devez activer votre compte avant la connexion !');
 
-            return new RedirectResponse('security_login');
+            return new RedirectResponse('security_logout');
+
 
         }
+
+
+
     }
 }
