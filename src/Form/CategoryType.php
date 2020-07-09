b4e269ac872fd02d41190bf070a45c1b1bdecc67
Nicolas <nicodupriez@hotmail.com>
2020-07-09 14:39:44 +0200

    Add CRUD for categories/able to add category when adding trick

---
+++ src/Form/CategoryType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'title',
+                TextType::class,
+                [
+                    'label' => 'Titre',
+                    'attr' => [
+                        'placeholder' => 'Titre de la catégorie',
+                    ],
+                ]
+            )
+            ->add(
+                'description',
+                TextareaType::class,
+                [
+                    'label' => 'Description',
+                    'attr' => [
+                        'placeholder' => 'Description du trick',
+                    ],
+                ]
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => Category::class,
+            ]
+        );
+    }
+}
