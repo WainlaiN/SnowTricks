4a2bdc2736f087c56697c9f8d20cbc70702f9ca5
Nicolas <nicodupriez@hotmail.com>
2020-08-21 17:41:11 +0200

    Working unit Test on Slugify with fixed Accent

--- src/Services/Slugify.php
+++ src/Services/Slugify.php
@@ -6,6 +6,9 @@ class Slugify
 {
     public static function slugify($str, $delimiter = '-')
     {
+        $accents = '/&([A-Za-z]{1,2})(grave|acute|circ|cedil|uml|lig);/';
+        $string_encoded = htmlentities($str, ENT_NOQUOTES, 'UTF-8');
+        $str = preg_replace($accents, '$1', $string_encoded);
         $str = strtolower(trim($str));
         $str = preg_replace('/[^A-Za-z0-9-]+/', '-', $str);
         $str = preg_replace('/-+/', "-", $str);
--- tests/Service/SlugifyTest.php
+++ tests/Service/SlugifyTest.php
@@ -11,9 +11,13 @@ class SlugifyTest extends TestCase
 
     public function testSlugify()
     {
-        $slugify = $this->createMock(Slugify::class);
+        //$slugify = $this->createMock(Slugify::class);
+        $slugify = New Slugify();
         $string = "Une chaine de caractère à transformer en slug";
-        $slugify->method('slugify');
+        $slugString = "une-chaine-de-caractere-a-transformer-en-slug";
+        $result = $slugify->slugify($string);
+        //dump($result);
+        $this->assertEquals($result, $slugString);
 
 
 
