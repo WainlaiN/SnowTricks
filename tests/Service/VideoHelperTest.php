e073ff8c155a8d1e690e85cfeeae1fc56cf137bf
Nicolas <nicodupriez@hotmail.com>
2020-08-25 17:50:15 +0200

    Added VideoHelperTest

--- tests/Service/VideoHelperTest.php
+++ tests/Service/VideoHelperTest.php
@@ -26,6 +26,8 @@ class VideoHelperTest extends TestCase
         $string = "https://www.dailymotion.com/video/xcegta";
         $string2 = "https://dai.ly/xcegta";
         $correctDailymotionUrl = "https://www.dailymotion.com/embed/video/xcegta";
+        $correctDailymotionUrl = "<div style=\"position:relative;padding-bottom:56.25%;height:0;overflow:hidden;\"> <iframe style=\"width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden\" frameborder=\"0\" type=\"text/html\" src=\"https://www.dailymotion.com/embed/video/xcegta\" width=\"100%\" height=\"100%\" allowfullscreen > </iframe> </div>
+";
 
         $videoHelper = new VideoHelper();
         $this->assertEquals($videoHelper->extractPlatformFromURL($string), $correctDailymotionUrl);
