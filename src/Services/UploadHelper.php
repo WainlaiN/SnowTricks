e26ec578624e36acbe80b6a5031df58259889575
Nicolas <nicodupriez@hotmail.com>
2020-08-25 18:02:49 +0200

    refactoring

--- src/Services/UploadHelper.php
+++ src/Services/UploadHelper.php
@@ -24,18 +24,14 @@ class UploadHelper
     public function saveImage($image): Image
     {
 
-        //get the image directory
         $destination = $this->uploadPath.self::IMAGE_DIR;
-        // get the uploaded file from the Image object
         $file = $image->getFile();
-        // set a filename
         $filename = md5(uniqid()) . '.' . $file->guessExtension();
-        // move the file
         $file->move(
             $destination,
             $filename
         );
-        //set the new filename to image
+
         $image->setImageFilename($filename);
 
         return $image;
@@ -43,11 +39,8 @@ class UploadHelper
 
     public function saveMainFile(File $file)
     {
-        //get the image directory
         $destination = $this->uploadPath.self::IMAGE_DIR;
-        // set a filename
         $filename = md5(uniqid()) . '.' . $file->guessExtension();
-        // move the file
         $file->move(
             $destination,
             $filename
@@ -58,11 +51,8 @@ class UploadHelper
 
     public function savePicture(File $file)
     {
-        //get the Picture directory
         $destination = $this->uploadPath.self::PICTURE_DIR;
-        // set a filename
         $filename = md5(uniqid()) . '.' . $file->guessExtension();
-        // move the file
         $file->move(
             $destination,
             $filename
