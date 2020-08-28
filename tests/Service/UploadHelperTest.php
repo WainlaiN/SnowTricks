259cc362c71c315ef737b1cf8889843b38d38f14
Nicolas <nicodupriez@hotmail.com>
2020-08-28 21:32:11 +0200

    working on unit test

--- tests/Service/UploadHelperTest.php
+++ tests/Service/UploadHelperTest.php
@@ -3,11 +3,27 @@
 namespace App\Tests\Service;
 
 use PHPUnit\Framework\TestCase;
+use App\Services\UploadHelper;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class UploadHelperTest extends TestCase
 {
-    public function testSomething()
+    /**public function testSomething()
     {
         $this->assertTrue(true);
+    }**/
+
+
+    public function testSaveImage ()
+    {
+        $photo = new UploadedFile(
+            '',
+            'photo.jpg',
+            'image/jpeg',
+            null
+        );
+
+        $uploadHelper = new UploadHelper("test");
+
     }
 }
