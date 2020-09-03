47f671245599894a66aa64a81da5dc59d90b6a46
Nicolas <nicodupriez@hotmail.com>
2020-09-03 22:45:05 +0200

    Controller have been refactored with TrickService

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -28,9 +28,6 @@ class TrickController extends AbstractController
     /**
      * @Route("/trick/new", name="trick_create")
      * @IsGranted("ROLE_USER")
-     * @param Request $request
-     * @param TrickService $trickService
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
     public function trickCreate(Request $request, TrickService $trickService)
     {
@@ -60,11 +57,6 @@ class TrickController extends AbstractController
     /**
      * @Route("/trick/{slug}/edit", name="trick_edit")
      * @IsGranted("ROLE_USER")
-     * @param Request $request
-     * @param TrickService $trickService
-     * @param TrickRepository $repo
-     * @param $slug
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
     public function trickEdit(Request $request, TrickService $trickService, TrickRepository $repo, $slug)
     {
@@ -93,11 +85,6 @@ class TrickController extends AbstractController
 
     /**
      * @Route("/trick/{slug}", name="trick_show")
-     * @param Request $request
-     * @param TrickService $trickService
-     * @param TrickRepository $repo
-     * @param $slug
-     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function trickShow(Request $request, TrickService $trickService, TrickRepository $repo, $slug)
     {
@@ -126,11 +113,6 @@ class TrickController extends AbstractController
     /**
      * @Route("/trick/{slug}/delete", name="trick_delete")
      * @IsGranted("ROLE_USER")
-     * @param TrickRepository $repo
-     * @param Request $request
-     * @param EntityManagerInterface $manager
-     * @param $slug
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
     public function delete(TrickRepository $repo, Request $request, EntityManagerInterface $manager, $slug)
     {
@@ -146,10 +128,6 @@ class TrickController extends AbstractController
     /**
      * @Route("/delete/image/{id}", name="image_delete", methods={"DELETE"})
      * @IsGranted("ROLE_USER")
-     * @param Image $image
-     * @param Request $request
-     * @param EntityManagerInterface $manager
-     * @return JsonResponse
      */
     public function deleteImage(Image $image, Request $request, EntityManagerInterface $manager)
     {
@@ -170,10 +148,6 @@ class TrickController extends AbstractController
     /**
      * @Route("/delete/video/{id}", name="video_delete", methods={"DELETE"})
      * @IsGranted("ROLE_USER")
-     * @param Video $video
-     * @param Request $request
-     * @param EntityManagerInterface $manager
-     * @return JsonResponse
      */
     public function deleteVideo(Video $video, Request $request, EntityManagerInterface $manager)
     {
@@ -186,197 +160,4 @@ class TrickController extends AbstractController
         //json response
         return new JsonResponse(['success' => 1]);
     }
-
-    /**
-     * @Route("/trick2/{slug}", name="trick_show2")
-     * @param TrickRepository $repo
-     * @param Request $request
-     * @param EntityManagerInterface $manager
-     * @param $slug
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    /**public function show(TrickRepository $repo, Request $request, EntityManagerInterface $manager, $slug)
-    {
-        $trick = $repo->findOneBySlug($slug);
-        $comment = new Comment();
-        $formComment = $this->createForm(CommentType::class, $comment);
-
-        $formComment->handleRequest($request);
-
-        if ($formComment->isSubmitted() && $formComment->isValid()) {
-            $comment->setCreatedAt(new \DateTime());
-            $comment->setTrick($trick);
-            $comment->setUser($this->getUser());
-            $manager->persist($comment);
-            $manager->flush();
-
-            $this->addFlash(
-                'success',
-                'Votre commentaire a bien été enregistré !'
-            );
-        }
-
-        return $this->render(
-            'trick/show.html.twig',
-            [
-                'trick' => $trick,
-                'formComment' => $formComment->createView(),
-            ]
-        );
-    }/**
-
-
-
-
-    /**
-     * @Route("/trick/{slug}/edit2", name="trick_edit2")
-     * @IsGranted("ROLE_USER")
-     */
-    /**public
-     * function edit(
-     * Request $request,
-     * EntityManagerInterface $manager,
-     * UploadHelper $uploadHelper,
-     * TrickRepository $repo,
-     * $slug,
-     * VideoHelper $videoHelper
-     * ) {
-     *
-     * $trick = $repo->findOneBySlug($slug);
-     * $form = $this->createForm(TrickType::class, $trick);
-     * $form->handleRequest($request);
-     *
-     * if ($form->isSubmitted() && $form->isValid()) {
-     *
-     * foreach ($trick->getImages() as $image) {
-     *
-     * //check if it's a new uploaded file
-     * if ($image->getFile()) {
-     * $image = $uploadHelper->saveImage($image);
-     * $image->setTrick($trick);
-     * $manager->persist($image);
-     * }
-     * }
-     *
-     * foreach ($trick->getVideos() as $video) {
-     *
-     * if ($video->getVideoURL()) {
-     *
-     * if ($videoHelper->extractPlatformFromURL($video->getVideoURL()) !== false) {
-     *
-     * $video->setVideoURL($videoHelper->extractPlatformFromURL($video->getVideoURL()));
-     * $video->setTrick($trick);
-     * $manager->persist($video);
-     *
-     * } else {
-     *
-     * $this->addFlash(
-     * 'danger',
-     * "Problème lors de l'enregistrement des videos"
-     * );
-     *
-     * return $this->redirectToRoute('trick_edit', ['slug' => $trick->getSlug()]);
-     *
-     * }
-     * }
-     * }
-     *
-     * $trick->setUserId($this->getUser());
-     *
-     * $uploadedMain = $form->get('file')->getData();
-     *
-     * if ($uploadedMain) {
-     * $mainImage = $uploadHelper->saveMainFile($uploadedMain);
-     * $trick->setMainImage($mainImage);
-     * }
-     *
-     * $trick->setUpdatedAt(new \DateTime());
-     * $manager->persist($trick);
-     * $manager->flush();
-     *
-     * $this->addFlash(
-     * 'success',
-     * 'Votre article a bien été modifié !'
-     * );
-     *
-     * return $this->redirectToRoute('trick_show', ['slug' => $trick->getSlug()]);
-     * }
-     *
-     * return $this->render(
-     * 'trick/edit.html.twig',
-     * [
-     * 'formTrick' => $form->createView(),
-     * 'trick' => $trick,
-     *
-     * ]
-     * );
-     * }**/
-
-    /**
-     * @Route("/trick/new2", name="trick_create2")
-     * @IsGranted("ROLE_USER")
-     */
-
-    /**public function create(
-     * Request $request,
-     * EntityManagerInterface $manager,
-     * UploadHelper $uploadHelper,
-     * VideoHelper $videoHelper
-     * ) {
-     *
-     * $trick = new Trick();
-     *
-     * $form = $this->createForm(TrickType::class, $trick);
-     * $form->handleRequest($request);
-     *
-     * if ($form->isSubmitted() && $form->isValid()) {
-     *
-     * $trick->setCreatedAt(new \DateTime());
-     * $UploadedMain = $form->get('file')->getData();
-     * $mainImage = $uploadHelper->saveMainFile($UploadedMain);
-     * $trick->setMainImage($mainImage);
-     * $trick->setUserId($this->getUser());
-     *
-     * foreach ($trick->getImages() as $image) {
-     * $image = $uploadHelper->saveImage($image);
-     * $image->setTrick($trick);
-     * }
-     *
-     * foreach ($trick->getVideos() as $video) {
-     *
-     * if ($videoHelper->extractPlatformFromURL($video->getVideoURL()) !== false) {
-     *
-     * $video->setVideoURL($videoHelper->extractPlatformFromURL($video->getVideoURL()));
-     * $video->setTrick($trick);
-     * $manager->persist($video);
-     *
-     * } else {
-     *
-     * $this->addFlash(
-     * 'danger',
-     * "Problème lors de l'enregistrement des videos"
-     * );
-     *
-     * return $this->redirectToRoute('trick_create');
-     * }
-     * }
-     *
-     * $manager->persist($trick);
-     * $manager->flush();
-     *
-     * $this->addFlash(
-     * 'success',
-     * 'Votre article a bien été ajouté !'
-     * );
-     *
-     * return $this->redirectToRoute('trick_show', ['slug' => $trick->getSlug()]);
-     * }
-     *
-     * return $this->render(
-     * 'trick/create.html.twig',
-     * ['formTrick' => $form->createView()]
-     * );
-     *
-     * }**/
-
 }
